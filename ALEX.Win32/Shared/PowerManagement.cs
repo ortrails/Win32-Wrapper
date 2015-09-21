a71592f19bc6a79ea65b677dfdff6e1932d24ef7
Alexander Witteveen <alex.witteveen@live.nl>
2015-09-21 02:16:45 +0200

    Added dll, winmd and test apps. Added initial PowerManagement class

---
+++ ALEX.Win32/Shared/PowerManagement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace ALEX.Win32
+{
+    public sealed class PowerManagement 
+    {
+        public static Guid GetActiveSchemeGuid()
+        {
+            var activeGuidPtr = IntPtr.Zero;
+
+            var result = PowerGetActiveScheme(IntPtr.Zero, ref activeGuidPtr);
+            if (result != SystemErrorCodes.ERROR_SUCCESS)
+            {
+                Debug.Assert(activeGuidPtr == IntPtr.Zero);
+                throw new Exception("PowerGetActiveScheme; Error:" + result);
+            }
+
+            var activeGuid = Marshal.PtrToStructure<Guid>(activeGuidPtr);
+            Marshal.FreeHGlobal(activeGuidPtr);
+            return activeGuid;
+        }
+
+        enum SystemErrorCodes : UInt32
+        {
+            ERROR_SUCCESS = 0,
+            ERROR_NO_MORE_ITEMS = 259
+        }
+
+        [DllImport("powrprof.dll")]
+        static extern SystemErrorCodes PowerGetActiveScheme(IntPtr RootPowerKey, ref IntPtr ActivePolicyGuid);
+    }
+}
---
+++ ALEX.Win32/TestConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using ALEX.Win32;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("GetActiveSchemeGuid : " + PowerManagement.GetActiveSchemeGuid());
+        }
+    }
+}

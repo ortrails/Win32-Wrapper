606552f62c917ca489934bdb59da8838cccfe84b
Alexander Witteveen <alex.witteveen@live.nl>
2015-09-21 18:11:40 +0200

    Separated Power Management

--- Scripts/BuildNuGet.ps1
+++ Scripts/BuildNuGet.ps1
@@ -1,11 +1,11 @@
-﻿$nuspec = (Split-Path -Parent $PSScriptRoot) + "\ALEX.Win32\ALEX.Win32.nuspec"
+﻿$nuspec = (Split-Path -Parent $PSScriptRoot) + "\ALEX.Win32.PowerManagement\ALEX.Win32.PowerManagement.nuspec"
 $output = (Split-Path -Parent $PSScriptRoot) + "\NuGet"
 $nuget = $PSScriptRoot + "\nuget.exe"
 $nuspec
 
 #&($nuget) "setApiKey" "4cfc0f8e-6ff5-475d-a212-f8afd6c26e1b"
 
-$result = (&($nuget) "pack" $nuspec -Prop "Configuration=Release" -Build -output "$output")
+$result = (&($nuget) "pack" "$nuspec" -Prop "Configuration=Release" -Build -output "$output")
 
 $package = ($result | where {$_.Contains("nupkg")} | foreach {
     $start = $_.IndexOf("'")

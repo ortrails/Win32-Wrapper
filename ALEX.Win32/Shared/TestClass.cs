8b2216ab9369fa58f20f6690f8dd6687dc7cdcfe
Alexander Witteveen <alex.witteveen@live.nl>
2015-09-21 02:37:46 +0200

    Commit solution

---
+++ ALEX.Win32/Shared/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+using ALEX.Win32;
+
+namespace ALEX.Win32
+{
+    public sealed class TestClass 
+    {
+        public TestClass()
+        {
+            Debug.WriteLine("GetActiveSchemeGuid : " + PowerManagement.GetActiveSchemeGuid());
+        }
+    }
+}

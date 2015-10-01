ac59ea8de286fd78a719f594d5e899268d0dca3f
Alexander Witteveen <alex.witteveen@live.nl>
2015-10-01 19:58:08 +0200

    Update

--- Scripts/BuildNuGet.ps1
+++ Scripts/BuildNuGet.ps1
@@ -4,5 +4,5 @@ $nuspec3 = (Split-Path -Parent $PSScriptRoot) + "\ALEX.Win32\ALEX.Win32.winmd\AL
 $output = (Split-Path -Parent $PSScriptRoot) + "\NuGet"
 $nuget = $PSScriptRoot + "\nuget.exe"
 $nuspec
-&($nuget) "pack" $nuspec2 -Prop "Configuration=Release" -Build -output "$output"
+&($nuget) "pack" $nuspec2 -Prop "Configuration=Release" -Build -OutputDirectory "$output"
 
